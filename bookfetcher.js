cd32bb939eb3c4aaebce104421621ee41583fa7b
entropy88 <entropystarrover@gmail.com>
2022-03-30 12:16:46 +0300

    added fallback cover image

--- bookfetcher.js
+++ bookfetcher.js
@@ -4,137 +4,150 @@ let result = document.getElementById("result");
 let biblio = document.getElementById('biblio');
 let cover = document.getElementById('coverImg');
 
-let isbn="";
+let isbn = "";
 
 console.log(fetchBtn)
 console.log(inputField)
 
 
 fetchBtn.addEventListener('click', function () {
-   isbn = inputField.value;
-    // fetchBook(isbn);
-    fetchGoogleBookVolumeId(isbn);
+  isbn = inputField.value;
+  // fetchBook(isbn);
+  fetchGoogleBookVolumeId(isbn);
 })
 
-function fetchGoogleBookVolumeId(isbn){
-    console.log(isbn, 'from google search')
+function fetchGoogleBookVolumeId(isbn) {
+  console.log(isbn, 'from google search')
 
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
-  .then(response => {
-    // indicates whether the response is successful (status code 200-299) or not
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${reponse.status}`)
-    }
-    return response.json()
-  })
-  .then(data => {  
-      let volumeId=data.items[0].id;
-      if (!volumeId){
-          result.innerText="няма резултати в базата :(";
-        
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
+    .then(response => {
+      // indicates whether the response is successful (status code 200-299) or not
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${reponse.status}`)
+      }
+      return response.json()
+    })
+    .then(data => {
+      let volumeId = data.items[0].id;
+      if (!volumeId) {
+        result.innerText = "няма резултати в базата :(";
+
       }
       console.log(volumeId);
       fetchInfo(volumeId);
 
-  })
-  .catch(error => {
-      console.log(error);
-    return;
-}
-  )
-}
-
-
-function fetchInfo(volumeId){   
-
-    fetch(`https://www.googleapis.com/books/v1/volumes/${volumeId}`)
-  .then(response => {
-    // indicates whether the response is successful (status code 200-299) or not
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${reponse.status}`)
-    }
-    return response.json()
-  })
-  .then(data => {  
-       let info=data.volumeInfo;  
-        let bookInfo=Object.assign(info);
-        visualise(bookInfo);  
-
-  })
-  .catch(error => console.log(error))
-}
-
-
-function visualise(bookInfo){
-  console.log(bookInfo)
-
-  //get image
-  let coverSrc=bookInfo.imageLinks.thumbnail;
-
-    let title=bookInfo.title;
-    let subtitle=bookInfo.subtitle;
-    let authors=bookInfo.authors.join(", ");
-    let publisher=bookInfo.publisher;    
-
-    //get the year only
-    let rawDate=bookInfo.publishedDate;
-    let publishedDateArray=rawDate.match(/\d{4}/);
-    let publishedDate=publishedDateArray[0];
-
-
-    let pages=bookInfo.pageCount;
-
-    cover.src=coverSrc;
-
-    let authorsP=document.createElement('p');
-    authorsP.innerText=authors;
-
-    let titleP=document.createElement('p');
-    titleP.textContent=title;
-
-    let subtitleP=document.createElement('p');
-   subtitleP.textContent=subtitle;
-
-    let publisherP=document.createElement('p');
-   publisherP.textContent=publisher;
-
-   let publishedP=document.createElement('p');
-   publishedP.textContent=publishedDate;
-
-   let pagesP=document.createElement('p');
-   pagesP.textContent=pages+" с.";
-
-
-
-    biblio.appendChild(authorsP);
-    biblio.appendChild(titleP);
-    biblio.appendChild(subtitleP);   
-    biblio.appendChild(publisherP);
-    biblio.appendChild(publishedP);
-    biblio.appendChild(pagesP);
-
-    //invert authors and capitalize
-    let recordAuthors=[];
-    bookInfo.authors.forEach(a=>{
-      let authorNames=a.split(" ");
-      console.log(authorNames);
-      let surname=authorNames[authorNames.length-1].toUpperCase();
-      console.log(surname);
-      let invertedName=surname;
-      authorNames.pop();
-      console.log(authorNames);
-      invertedName+=`, ${authorNames.join(" ")}`;
-      //remove . if invertedName ends on it
-      if (invertedName.charAt(invertedName.length - 1) == '.') {
-        invertedName = invertedName.substr(0, invertedName.length - 1);
-      }
-
-      recordAuthors.push(invertedName);
     })
+    .catch(error => {
+      console.log(error);
+      return;
+    })
+}
 
-    let record=`${recordAuthors.join(', ')}. ${title}. ${publisher}, ${publishedDate}. ISBN ${isbn}`;
-    let recordP=document.createElement('p');
-    recordP.innerText=record;
-    result.appendChild(recordP)
+
+function fetchInfo(volumeId) {
+
+  fetch(`https://www.googleapis.com/books/v1/volumes/${volumeId}`)
+    .then(response => {
+      // indicates whether the response is successful (status code 200-299) or not
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${reponse.status}`)
+      }
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      let isEbook=data.saleInfo.isEbook;
+      console.log(isEbook)
+      let info = data.volumeInfo;
+      let bookInfo = Object.assign(info);
+      visualise(bookInfo);
+
+    })
+    .catch(error => console.log(error))
+}
+
+
+function visualise(bookInfo) {
+ //clean container
+ biblio.innerHTML="";
+
+  //get image if there is one
+  let coverSrc="./no_cover.png";
+  if (bookInfo.imageLinks){
+coverSrc = bookInfo.imageLinks.thumbnail;
+  }
+ 
+
+  let title = bookInfo.title;
+  let subtitle = bookInfo.subtitle;
+  let authors = bookInfo.authors.join(", ");
+  let publisher = bookInfo.publisher;
+
+  //get the year only
+  let rawDate = bookInfo.publishedDate;
+  let publishedDateArray = rawDate.match(/\d{4}/);
+  let publishedDate = publishedDateArray[0];
+
+
+  let pages = bookInfo.pageCount;
+
+  cover.src = coverSrc;
+
+  let authorsP = document.createElement('p');
+  authorsP.className="info_authors";
+  authorsP.innerText = authors;
+
+  let titleP = document.createElement('p');
+  titleP.textContent = title;
+
+  let subtitleP = document.createElement('p');
+  subtitleP.textContent = subtitle;
+
+  let publisherP = document.createElement('p');
+  publisherP.className="info_publishingDetails";
+  publisherP.textContent = publisher;
+
+  let publishedP = document.createElement('p');
+  publishedP.className="info_publishingDetails";
+  publishedP.textContent = publishedDate;
+
+  let pagesP = document.createElement('p');
+  pagesP.className="info_publishingDetails";
+  pagesP.textContent = pages + " с.";
+
+
+
+  biblio.appendChild(authorsP);
+  biblio.appendChild(titleP);
+  biblio.appendChild(subtitleP);
+  biblio.appendChild(publisherP);
+  biblio.appendChild(publishedP);
+  biblio.appendChild(pagesP);
+
+  //invert authors and capitalize
+  let recordAuthors = [];
+  bookInfo.authors.forEach(a => {
+    let authorNames = a.split(" ");
+    let surname = authorNames[authorNames.length - 1].toUpperCase();
+    let invertedName = surname;
+    authorNames.pop();
+    invertedName += `, ${authorNames.join(" ")}`;
+    //remove . if invertedName ends on it
+    if (invertedName.charAt(invertedName.length - 1) == '.') {
+      invertedName = invertedName.substr(0, invertedName.length - 1);
+    }
+
+    recordAuthors.push(invertedName);
+  })
+
+  //add subtitle if any
+  if (subtitle){
+    title+=`: ${subtitle}`
+  }
+
+  let record = `${recordAuthors.join(', ')}. ${title}. ${publisher}, ${publishedDate}. ISBN ${isbn}`;
+  let recordP = document.createElement('p');
+  recordP.innerText = record;
+  result.appendChild(recordP)
 
 }
