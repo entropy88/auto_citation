bcf336a8a20931f6ef1af383d0adc88c9f28c2d1
entropy88 <entropystarrover@gmail.com>
2022-04-16 14:30:17 +0300

    exporting as doc and monkeying around with ui

--- bookfetcher.js
+++ bookfetcher.js
@@ -8,6 +8,7 @@ let cover = document.getElementById('coverImg');
 let addToBibliographyButton = document.getElementById('addToBibliography');
 let bibliographyContainer = document.getElementById('bibliographyContainer');
 let italics=document.getElementById('italics');
+let exportButton=document.getElementById("export")
 
 
 let isbn = "";
@@ -252,4 +253,41 @@ function visualise(bookInfo) {
      italics.appendChild(r.htmle)
     })
   }
+}
+
+
+function Export2Doc(element, filename = ''){
+  let preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+  let postHtml = "</body></html>";
+  let html = preHtml+document.getElementById(element).innerHTML+postHtml;
+
+  let blob = new Blob(['\ufeff', html], {
+      type: 'application/msword'
+  });
+  
+  // Specify link url
+  let url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+  
+  // Specify file name
+  filename = filename?filename+'.doc':'document.doc';
+  
+  // Create download link element
+  let downloadLink = document.createElement("a");
+
+  document.body.appendChild(downloadLink);
+  
+  if(navigator.msSaveOrOpenBlob ){
+      navigator.msSaveOrOpenBlob(blob, filename);
+  }else{
+      // Create a link to the file
+      downloadLink.href = url;
+      
+      // Setting the file name
+      downloadLink.download = filename;
+      
+      //triggering the function
+      downloadLink.click();
+  }
+  
+  document.body.removeChild(downloadLink);
 }
